7eeb8548487ca0d0a6aa67997df1b53351a9ce8d
André C. Andersen <andre@andersen.im>
2025-02-18 01:47:14 +0100

    Update __init__.py

--- src/gnistai/__init__.py
+++ src/gnistai/__init__.py
@@ -1,2 +1,11 @@
+import os
+
 class GnistAI:
-    pass
+    def __init__(api_key: str | None = None):
+        if api_key:
+            self.api_key = api_key
+        else:
+            self.api_key = os.environ["GNISTAI_API_KEY"]
+    
+    def health_check():
+        raise NotImplementedError()
